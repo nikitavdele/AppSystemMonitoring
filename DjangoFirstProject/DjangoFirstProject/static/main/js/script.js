e65e27bb0a1bea2573fa22e61034f3635f79bc1b
Nikita <kit130803@gmail.com>
2024-07-10 14:04:34 +0300

    commit script test

--- DjangoFirstProject/DjangoFirstProject/static/main/js/script.js
+++ DjangoFirstProject/DjangoFirstProject/static/main/js/script.js
@@ -6,3 +6,30 @@ document.addEventListener('DOMContentLoaded', function() {
 
 let temp = document.getElementById('temp')
 temp.set-attribute('value', 10)
+
+
+let loadaverage = fetch(url, [options])
+let response = await fetch(url);
+
+if (response.ok) { // если HTTP-статус в диапазоне 200-299
+  // получаем тело ответа (см. про этот метод ниже)
+  let json = await response.json();
+} else {
+  alert("Ошибка HTTP: " + response.status);
+}
+
+//let loadaverage = {
+//  name: 'John',
+//  surname: 'Smith'
+//};
+//
+//let response = await fetch('/article/fetch/post/loadaverage', {
+//  method: 'POST',
+//  headers: {
+//    'Content-Type': 'application/json;charset=utf-8'
+//  },
+//  body: JSON.stringify(loadaverage)
+//});
+//
+//let result = await response.json();
+//alert(result.message);

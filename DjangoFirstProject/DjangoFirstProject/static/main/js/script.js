831bc44693c59068007fb0416be7b1f184720e09
Nikita <kit130803@gmail.com>
2024-07-10 12:58:11 +0300

    commit script match html

---
+++ DjangoFirstProject/DjangoFirstProject/static/main/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("123");
+
+    // Другой код здесь
+});
+
+let temp = document.getElementById('temp')
+temp.set-attribute('value', 10)

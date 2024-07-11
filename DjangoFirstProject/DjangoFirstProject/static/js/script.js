ce086898e10e5a966c9b30d73c6abf6634b55289
Nikita <kit130803@gmail.com>
2024-07-11 15:07:28 +0300

    commit fetch memFree

--- DjangoFirstProject/DjangoFirstProject/static/js/script.js
+++ DjangoFirstProject/DjangoFirstProject/static/js/script.js
@@ -4,27 +4,33 @@ document.addEventListener('DOMContentLoaded', function() {
     // Другой код здесь
 });
 
-let temp = document.getElementById('temp')
-console.log(temp.innerHTML)
-temp.innerHTML = "16%"
+//let temp = document.getElementById('temp')
+//console.log(temp.innerHTML)
+//temp.innerHTML = "16%"
 
-try {
-  let response = await fetch('/get_params');
+fetch('/get_params', { method: 'GET' }).then(res => res.json()).then(function(data_names) {
+             console.log(data_names.MemFree)
 
-  if (response.ok) {
-    let data = await response.json();
+        })
 
-    if (data.cpu_cores !== undefined) {
-      document.getElementById('cpu_cores').innerText = `CPU Cores: ${data.cpu_cores}`;
-    } else {
-      console.error("Invalid data format", data);
-    }
-  } else {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-} catch (error) {
-  console.error("An error occurred:", error);
-}
+//try {
+//  let response = fetch('/get_params');
+//
+//  if (response.ok) {
+//    let data = response.json();
+//    console.log(data)
+//
+//    if (data.cpu_cores !== undefined) {
+//      document.getElementById('cpu_cores').innerText = `CPU Cores: ${data.cpu_cores}`;
+//    } else {
+//      console.error("Invalid data format", data);
+//    }
+//  } else {
+//    throw new Error(`HTTP error! Status: ${response.status}`);
+//  }
+//} catch (error) {
+//  console.error("An error occurred:", error);
+//}
 
 
 // fetch('/get_params')
